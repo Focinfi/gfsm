d60d41ee3800638ef888fbd1f910d84a17d0517e
Focinfi <focinfi@gmail.com>
2016-01-19 12:50:29 +0800

    Comment: Add comments for gfsm.go

--- gfsm.go
+++ gfsm.go
@@ -5,15 +5,20 @@ import (
 	"github.com/Focinfi/gset"
 )
 
+// State has pointer of StateMachine tagged with `sql:"-"`
+// which means ingore this column when using github.com/jinzhu/gorm
 type State struct {
 	currentState string
 	sm           *StateMachine `sql:"-"`
 }
 
+// CurrentState return this state's state
 func (s State) CurrentState() string {
 	return string(s.currentState)
 }
 
+// Event has the pointer of the StateMachine to which this event bolonging
+// one event triggerred from some state to one state
 type Event struct {
 	name  string
 	sm    *StateMachine
@@ -21,15 +26,18 @@ type Event struct {
 	to    string
 }
 
+// StateMachine has a set of states and events
 type StateMachine struct {
 	events map[string]*Event
 	states *gset.Set
 }
 
+// NewState return a pointer of new state
 func NewState(currentState string, sm *StateMachine) *State {
 	return &State{currentState, sm}
 }
 
+// NewStateMachine return a pointer of new StateMachine
 func NewStateMachine(states ...string) *StateMachine {
 	stateE := make([]gset.Elementer, len(states))
 	for i, state := range states {
@@ -39,6 +47,7 @@ func NewStateMachine(states ...string) *StateMachine {
 	return &StateMachine{make(map[string]*Event), stateSet}
 }
 
+// Event find or create a event
 func (sm *StateMachine) Event(name string) *Event {
 	event, ok := sm.events[name]
 	if !ok {
@@ -48,6 +57,8 @@ func (sm *StateMachine) Event(name string) *Event {
 	return event
 }
 
+// Transition add transition for this event, error will not be nil
+// if any state in to or froms does not exit
 func (e *Event) Transition(to string, froms ...string) error {
 	states := append(froms[:], to)
 
@@ -62,6 +73,10 @@ func (e *Event) Transition(to string, froms ...string) error {
 	return nil
 }
 
+// Trigger trigger event, error will not be nil if the event does not exist or
+// current state is not in this event's froms
+// s.State will be change to the event's to state, when event triggers sucessfully,
+// and error is nil
 func (s *State) Trigger(eventName string) error {
 	event, ok := s.sm.events[eventName]
 	if !ok {
